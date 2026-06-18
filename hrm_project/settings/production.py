0d6065a5de523c8dc5d8885addbdf0b05dcb3e0f
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-06-18 14:23:06 +0545

    feat: add production settings file with host and CORS configurations

--- hrm_project/settings/production.py
+++ hrm_project/settings/production.py
@@ -12,3 +12,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000/",
+    "http://127.0.0.1:3000/",
+    "https://hrm.darkmatterproduction.org/"
+]
